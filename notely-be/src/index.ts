24fab8f9c2fa0ed4fb77464c64ce528c2508b3b5
bersling <bersling@gmail.com>
2017-04-02 10:22:49 +0200

    wip

--- notely-be/src/index.ts
+++ notely-be/src/index.ts
@@ -1,12 +1,44 @@
-import * as express from 'express';
-import * as bodyParser from 'body-parser';
+import * as http from 'http';
+import * as debug from 'debug';
 
-const app = express();
+import App from './App';
 
-app.get('/', function (req, res) {
-  res.send('hello world')
-});
+debug('ts-express:server');
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
-});
+const port = normalizePort(process.env.PORT || 3000);
+App.set('port', port);
+
+const server = http.createServer(App);
+server.listen(port);
+server.on('error', onError);
+server.on('listening', onListening);
+
+function normalizePort(val: number|string): number|string|boolean {
+  let port: number = (typeof val === 'string') ? parseInt(val, 10) : val;
+  if (isNaN(port)) return val;
+  else if (port >= 0) return port;
+  else return false;
+}
+
+function onError(error: NodeJS.ErrnoException): void {
+  if (error.syscall !== 'listen') throw error;
+  let bind = (typeof port === 'string') ? 'Pipe ' + port : 'Port ' + port;
+  switch(error.code) {
+    case 'EACCES':
+      console.error(`${bind} requires elevated privileges`);
+      process.exit(1);
+      break;
+    case 'EADDRINUSE':
+      console.error(`${bind} is already in use`);
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+}
+
+function onListening(): void {
+  let addr = server.address();
+  let bind = (typeof addr === 'string') ? `pipe ${addr}` : `port ${addr.port}`;
+  debug(`Listening on ${bind}`);
+}
