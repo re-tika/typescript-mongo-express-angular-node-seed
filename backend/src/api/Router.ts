894220095eb8569fe9d52864da3c34b85301e63f
bersling <bersling@gmail.com>
2017-04-07 09:12:22 +0200

    update main repo

--- backend/src/api/Router.ts
+++ backend/src/api/Router.ts
@@ -1,11 +1,6 @@
-import * as path from 'path';
 import * as express from 'express';
-import * as logger from 'morgan';
 import * as bodyParser from 'body-parser';
 import {simpleCrudRouter} from './endpoints/SimpleCrudRouter';
-import session = require("express-session");
-//const RedisStore = require('connect-redis')(session);
-//import * as passport from 'passport';
 
 // Creates and configures an ExpressJS web server.
 class Router {
@@ -22,22 +17,8 @@ class Router {
 
   // Configure Express middleware.
   private middleware(): void {
-    this.express.use(logger('dev'));
     this.express.use(bodyParser.json());
     this.express.use(bodyParser.urlencoded({ extended: false }));
-
-    /* TODO: find problematic part, test
-    this.express.use(session({
-      store: new RedisStore({
-        url: appConfig.appConfig.redis.url
-      }),
-      secret: appConfig.appConfig.redis.secret,
-      resave: false,
-      saveUninitialized: false
-    }));
-    this.express.use(passport.initialize());
-    this.express.use(passport.session());
-    */
   }
 
   // Configure API endpoints.
@@ -53,7 +34,7 @@ class Router {
       });
     });
     this.express.use('/', router);
-    this.express.use('/api/v1/heroes', simpleCrudRouter);
+    this.express.use('/api/v1/', simpleCrudRouter);
   }
 
 }
--- backend/src/type-declarations.ts
+++
@@ -1,7 +0,0 @@
-let TYPES = {
-  User: Symbol("User"),
-  AppProperties: Symbol("AppProperties"),
-  Key: Symbol("Key")
-};
-
-export { TYPES };
